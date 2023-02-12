c1953069cab2b5528ce1a4945361371f51ee3369
razu <razujcs@gmail.com>
2023-02-12 12:11:20 +0600

    Update

--- Data Structure/Module 24/Queue_Array.cpp
+++ Data Structure/Module 24/Queue_Array.cpp
@@ -1,48 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX_SIZE = 500;
+const int MAX_SIZE = 500;//circular array 
 class Queue
 {
 public:
     int arr[MAX_SIZE];
     int l;
     int r;
+    int size;
     Queue()
     {
         l = 0;
         r = -1;
+        size = 0;
     }
     void enqueue(int val)
     {
-        if (r + 1 >= MAX_SIZE)
+        if (size == MAX_SIZE)
         {
             cout << "Queue is full";
             return;
         }
 
         r++;
+        if (r == MAX_SIZE)
+        {
+            r = 0;
+        }
         arr[r] = val;
+        size++;
     }
     void dequeue()
     {
-        if (l > r)
+        if (size == 0)
         {
             cout << "Queue is Enpty";
         }
         l++;
+
+        if (l == MAX_SIZE)
+        {
+            l = 0;
+        }
+        size--;
     }
     int front()
     {
-        if (l > r)
+        if (size == 0)
         {
             cout << "Queue is Enpty ";
             return -1;
         }
         return arr[l];
     }
-    int size()
+    int getsize()
     {
-        return r - l + 1;
+        return size;
     }
 };
 int main()
@@ -53,7 +66,7 @@ int main()
     q.enqueue(5);
     q.enqueue(5);
 
-    cout << q.size() << endl;
+    cout << q.getsize() << endl;
     cout << q.front() << endl;
     q.dequeue();
     cout << q.front() << endl;
@@ -63,5 +76,5 @@ int main()
     cout << q.front() << endl;
     q.dequeue();
     cout << q.front() << endl;
-    cout << q.size() << endl;
+    cout << q.getsize() << endl;
 }
---
+++ Data Structure/Module 24/dynamic Queue.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+using namespace std;
+// const int MAX_SIZE = 500; // circular array
+class Queue
+{
+public:
+    int *arr;
+    int array_cap;
+    int l;
+    int r;
+    int size;
+    Queue()
+    {
+        arr = new int[1];
+        array_cap = 1;
+        l = 0;
+        r = -1;
+        size = 0;
+    }
+
+    void remove_circular()
+    {
+        if (l > r)
+        {
+            int *temp = new int[array_cap];
+            int index = 0;
+            for (int i = l; i < array_cap; i++)
+            {
+                temp[index] = arr[i];
+                index++;
+            }
+            for (int i = 0; i <= r; i++)
+            {
+                temp[index] = arr[i];
+                index++;
+            }
+            swap(temp, arr);
+            l = 0;
+            r = array_cap - 1;
+
+            delete[] temp;
+        }
+    }
+
+    void increase_size()
+    {
+        remove_circular();
+        int *temp = new int[array_cap * 2];
+        for (int i = 0; i < array_cap; i++)
+            temp[i] = arr[i];
+        swap(arr, temp);
+        array_cap = array_cap * 2;
+        delete[] temp;
+    }
+
+    void enqueue(int val)
+    {
+        if (size == array_cap)
+        {
+            increase_size();
+        }
+
+        r++;
+        if (r == array_cap)
+        {
+            r = 0;
+        }
+        arr[r] = val;
+        size++;
+    }
+    void dequeue()
+    {
+        if (size == 0)
+        {
+            cout << "Queue is Enpty";
+        }
+        l++;
+
+        if (l == array_cap)
+        {
+            l = 0;
+        }
+        size--;
+    }
+    int front()
+    {
+        if (size == 0)
+        {
+            cout << "Queue is Enpty ";
+            return -1;
+        }
+        return arr[l];
+    }
+    int getsize()
+    {
+        return size;
+    }
+};
+int main()
+{
+    Queue q;
+    q.enqueue(5);
+    q.enqueue(5);
+    q.enqueue(5);
+    q.enqueue(5);
+
+    cout <<"Size : "<< q.getsize() << endl;
+    cout << q.front() << endl;
+    q.dequeue();
+    cout << q.front() << endl;
+    q.dequeue();
+    cout << q.front() << endl;
+    q.dequeue();
+    cout << q.front() << endl;
+    q.dequeue();
+    cout << q.front() << endl;
+    cout<<"Size : " << q.getsize() << endl;
+}
