b5a9c876aed8854d5503f7aabb218028a5f0dbee
razu <razujcs@gmail.com>
2023-03-17 12:02:00 +0600

    Update

---
+++ Algorithm/Module 6/1.cpp
@@ -0,0 +1,19 @@
+// Fibonacci Using Recursion
+#include <iostream>
+using namespace std;
+int fibonacci(int n)
+{
+    // base case
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    // general case
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << fibonacci(n) << endl;
+}
