0adcc9e0a48c3b89463b351758251f64222c595d
razu <razujcs@gmail.com>
2023-08-12 00:36:38 +0600

    Update

---
+++ Django/sixth_project/first_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Student (models.Model):
+    
+    name = models.CharField(max_length=50)
+    roll = models.IntegerField(primary_key=True)
+    address = models.TextField()
+    cell = models.IntegerField(default=123)
+    def __str__(self):
+        return f"Roll : {self.roll} - {self.name}"
+    
+    
