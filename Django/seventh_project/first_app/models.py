a61f301364d45d0d456516ca90518aac6a9ae69b
razu <razujcs@gmail.com>
2023-08-12 19:48:24 +0600

    Update

--- Django/seventh_project/first_app/models.py
+++ Django/seventh_project/first_app/models.py
@@ -8,3 +8,7 @@ class StudentModel(models.Model):
     name = models.CharField(max_length=20)
     father_name = models.CharField(max_length=20)
     address = models.TextField()
+    
+def __str__(self):
+    return f"Name :{self.name}"
+ 
